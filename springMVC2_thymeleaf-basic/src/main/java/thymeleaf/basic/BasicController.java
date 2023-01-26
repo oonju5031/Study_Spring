318575db330ff3c497a8b43ec0b6e452b580c49d
oonju5031 <oonju5031@gmail.com>
2023-01-26 13:37:51 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 Section 1.8 ~ 1.11

--- springMVC2_thymeleaf-basic/src/main/java/thymeleaf/basic/BasicController.java
+++ springMVC2_thymeleaf-basic/src/main/java/thymeleaf/basic/BasicController.java
@@ -92,6 +92,45 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
     @Data
     static class User {
         private String username;
