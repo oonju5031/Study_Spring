66cb349294e9bbfc2c5713a73499813fb942770e
이준영 <oonju99@snbplus.co.kr>
2023-02-10 17:37:45 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 Section 1.14

--- springMVC2_thymeleaf-basic/src/main/java/thymeleaf/basic/BasicController.java
+++ springMVC2_thymeleaf-basic/src/main/java/thymeleaf/basic/BasicController.java
@@ -134,6 +134,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("User\"A\"", 10));
+        model.addAttribute("escape", new User("UserA", 10));
+        addUsers(model);
+        return "/basic/javascript";
+    }
+
     private void addUsers(Model model) {
 
         List<User> list = new ArrayList<>();
