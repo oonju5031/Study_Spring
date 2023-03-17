4623e35a44362948e26150b485cc046fb4112732
oonju5031 <oonju5031@gmail.com>
2023-03-17 17:56:15 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 Section 5.2 ~ 5.7

--- springMVC2_validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ springMVC2_validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,28 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+/*@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량은 10,000원 이상이어야 합니다.")*/
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
