78962a7a8cd460e04a4a96521cadff8091f1fe78
JunYoung Lee <oonju5031@gmail.com>
2025-04-10 17:01:56 +0900

    [CHORE] Lombok 추가
    기존 DTO getter/setter에 lombok 적용

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,23 +1,15 @@
 package hello.hellospring.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Member {
 
     private Long id;
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
