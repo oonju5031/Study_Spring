8fc9ac7bd83e39d0de184b40d7bde21a4b5c142e
JunYoung Lee <oonju5031@gmail.com>
2025-04-10 17:02:22 +0900

    [FEAT] 6. 회원 관리 예제
    회원 등록, 회원 목록 열람 기능 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,12 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,4 +17,24 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String newMember(Model model) {
+        return "member-new";
+    }
+
+    @PostMapping("/members/new/submit")
+    public String submitNewMember(Member member) {
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members/list")
+    public String memberList(Model model) {
+        model.addAttribute("memberList", memberService.findMembers());
+
+        return "member-list";
+    }
+
 }
