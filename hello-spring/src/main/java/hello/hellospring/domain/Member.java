1b803ec8ccd1baea6b46e00c1561dbffc32db809
JunYoung Lee <oonju5031@gmail.com>
2025-05-01 13:25:17 +0900

    [FEAT] 7. 스프링 DB 접근 기술
    DB Connection 추가

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -4,11 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @ToString
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
